e85355c080b91e02b865c5159489d52d16f2b5f6
DanaeKontou <47666253+DanaeKontou@users.noreply.github.com>
2025-03-24 13:03:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// This is where we will put the map code later
-console.log("Map will be added here!");
+// Create the map
+var map = L.map('map').setView([0, 0], 2); // Centered on the world
+
+// Add a minimal base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap',
+  maxZoom: 18
+}).addTo(map);
+
+// Add a marker just for fun (optional)
+L.marker([0, 0]).addTo(map).bindPopup('Start exploring!').openPopup();
